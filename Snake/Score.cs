cb7675ea3b839bb71c6a2396a78aeef39608ff01
jamram <jamram@mail.ru>
2019-04-19 23:24:00 +0300

    Подсчет очков в2

---
+++ Snake/Score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Score
+    {
+        int score = 0;
+
+        public void Plus()
+        {
+            score++;
+        }
+
+        public int Show()
+        {
+            return score;
+        }
+    }
+}
